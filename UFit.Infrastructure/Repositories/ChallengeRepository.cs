f2485b48a45fac6a30432f1e64acbdceb7a500c6
Sergio Bazan <55011041+sergiobazan@users.noreply.github.com>
2024-11-16 13:19:58 -0500

    add workout decision to get a routine

---
+++ UFit.Infrastructure/Repositories/ChallengeRepository.cs
@@ -0,0 +1,16 @@
+﻿using UFit.Domain.Challenges;
+
+namespace UFit.Infrastructure.Repositories;
+
+internal class ChallengeRepository : IChallengeRepository
+{
+    public void Add(Challenge challenge)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Challenge?> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
